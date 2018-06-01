642f9efdad12fabab8cc9d0e671449084009a7c4
Steven Tebbs <sven_was_here@yahoo.com>
2018-06-01 16:12:18 -0600

    Implemented creation process for posts and refactored

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
+  	before do
+		visit posts_path
+  	end
     it 'can be reached successfully' do
-      visit posts_path
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
 
   describe 'creation' do
+  		before do
+	  		visit new_post_path
+		end
   	it 'has a new form that can be reached' do
-  		visit new_post_path
   		expect(page.status_code).to eq(200)
   	end
 
   	it 'can be created from new form page' do
-  	  visit new_post_path
 
 	  fill_in 'post[date]', with: Date.today
 	  fill_in 'post[rationale]', with: "Some rationale"
