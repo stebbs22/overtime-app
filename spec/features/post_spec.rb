c170c2160b5e1e7270ffa00751f1d45a478c82e8
Steven Tebbs <sven_was_here@yahoo.com>
2018-06-04 17:06:33 -0600

    Updated posts page with a table and index of posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
-    it 'can be reached successfully' do
-      visit posts_path
-      expect(page.status_code).to eq(200)
+  before do
+    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Smith")
+    login_as(user, :scope => :user)
     end
 
-    it 'has a title of Posts' do
+  describe 'index' do
+    before do
       visit posts_path
-      expect(page).to have_content(/Posts/)
+    end
+    
+      it 'can be reached successfully' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'has a title of Posts' do
+        expect(page).to have_content(/Posts/)
+      end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Smith")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
 
