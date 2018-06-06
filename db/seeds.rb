497c6608ca039c96063c519e5d99caad6da49640
Steven Tebbs <sven_was_here@yahoo.com>
2018-06-06 15:15:09 -0600

    customized administrate gem to display status of the posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Smith")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Steven", last_name: "Tebbs")
+
+puts "1 Admin User created"	
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
