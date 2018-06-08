2400512e1d82c5f9405eafaa08baf28081197c74
Steven Tebbs <sven_was_here@yahoo.com>
2018-06-08 16:22:07 -0600

    Updated show page for audit log. Implemented html format

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -16,5 +16,15 @@ describe 'AuditLog Feature' do
       visit audit_logs_path
       expect(page).to have_content(/SMITH/)
     end
+
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
