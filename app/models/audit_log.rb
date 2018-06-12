cd7fa1e0463283fca19d2e33d8e6f1038e75e029
Steven Tebbs <sven_was_here@yahoo.com>
2018-06-12 14:35:30 -0600

    Final changes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,10 +3,16 @@ class AuditLog < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_defaults
+  before_update :set_end_date, if: :confirmed?
+  scope :by_start_date, -> { order('start_date DESC') }
 
   private
 
 	  def set_defaults
 	  	self.start_date ||= Date.today - 6.days
+	  end
+
+	  def set_end_date
+	  	self.end_date = Date.today
 	  end
 end
